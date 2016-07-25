8658fc4408b025339a0b8820ccb237bca4b95b4f
ampirolli <ampirolli831@gmail.com>
2016-07-25 19:23:03 -0400

    week2 commit 1

--- app/src/main/java/seniorproject/smartstocks/LICS.java
+++ app/src/main/java/seniorproject/smartstocks/LICS.java
@@ -6,7 +6,7 @@ package seniorproject.smartstocks;
 public class LICS {
     public static String LoginConnectionString()
     {
-        String connection = "jdbc:sqlserver://localhost:1433;" + "databaseName=SE414_Group3;user=SE414_group3;password=ahanan";
+        String connection = "jdbc:jtds:sqlserver://sql.neit.edu;" + "databaseName=SE414_Group3;user=SE414_group3;password=ahanan";
         // "Server=sql.neit.edu;Database=SE417_AP;User Id=001329606;Password=001329606;";
         return connection;
     }
