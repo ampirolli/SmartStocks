f5b8958a40255378d1af29389b6f7dfe07b4b614
ampirolli <ampirolli831@gmail.com>
2016-07-31 15:41:21 -0400

    Registration needs to be finished/ connected to database. Stored Procedure is completed and working, just needs implementation finish home page + update designs

--- app/src/main/java/seniorproject/smartstocks/RegistrationActivity.java
+++ app/src/main/java/seniorproject/smartstocks/RegistrationActivity.java
@@ -2,12 +2,27 @@ package seniorproject.smartstocks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText txtEmailView;
+    private EditText txtPassword;
+    private EditText txtPasswordMatch;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        txtEmailView = (EditText) findViewById(R.id.txtEmail);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        txtPasswordMatch = (EditText) findViewById(R.id.txtPasswordMatch);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+
     }
 }
