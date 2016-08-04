808447ed27fa346ecaa4b57b4080efa3b74c5b3f
ampirolli <ampirolli831@gmail.com>
2016-08-04 14:49:25 -0400

    Registration needs to be finished/ connected to database. Stored Procedure is completed and working, just needs implementation finish home page + update designs

--- app/src/main/java/seniorproject/smartstocks/RegistrationActivity.java
+++ app/src/main/java/seniorproject/smartstocks/RegistrationActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,17 +33,93 @@ public class RegistrationActivity extends AppCompatActivity {
             public void onClick(View view) {
 
 
-                String email = txtEmail.getText().toString().toLowerCase();
-                String password = txtPassword.getText().toString();
-
-                AuthTask = new UserRegistrationEmailTask(email, password);
-                AuthTask.execute((Void) null);
+               attempCreation();
 
             }
         });
 
 
     }
+    private void attempCreation(){
+        if (AuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        txtEmail.setError(null);
+        txtPassword.setError(null);
+        txtPasswordMatch.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
+        String passwordMatch = txtPasswordMatch.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password) || !isPasswordValid(password)) {
+            txtPassword.setError(getString(R.string.error_invalid_password));
+            focusView = txtPassword;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            txtEmail.setError(getString(R.string.error_field_required));
+            focusView = txtEmail;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            txtEmail.setError(getString(R.string.error_invalid_email));
+            focusView = txtEmail;
+            cancel = true;
+        }
+
+
+        if (TextUtils.isEmpty(passwordMatch)) {
+            txtPasswordMatch.setError(getString(R.string.error_field_required));
+            focusView = txtPasswordMatch;
+            cancel = true;
+        }
+
+        if (!doPasswordsMatch(password, passwordMatch)) {
+            txtPasswordMatch.setError("These passwords do not match");
+            focusView = txtPasswordMatch;
+            cancel = true;
+        }
+
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            //showProgress(true);
+            AuthTask = new UserRegistrationEmailTask(email, password);
+            AuthTask.execute((Void) null);
+
+        }
+
+    }
+
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 7;
+    }
+
+    private boolean doPasswordsMatch(String password, String passwordMatch){
+        return password.equals(passwordMatch);
+    }
+
 
     public class UserRegistrationEmailTask extends AsyncTask<Void, Void, Boolean> {
 
@@ -72,28 +149,12 @@ public class RegistrationActivity extends AppCompatActivity {
 
             if (success) {
 
-                if (Password.equals(txtPasswordMatch.getText().toString())) {
-
-                    if (Password.length() > 7) {
                         Intent i = new Intent(RegistrationActivity.this, RegistrationPersonalInfoActivity.class).putExtra(Email, Password); //creates intent that launches personal info page
                         startActivity(i);
 
-
-                    } else {
-                        txtPassword.setError("Password length is too short. ");
-                        txtPassword.requestFocus();
-                        //password length is too short!
-                    }
-
-                } else {
-                    txtPassword.setError("Passwords do not match. ");
-                    txtPassword.requestFocus();
-                }
-
-
             } else {
 
-                txtEmail.setError("This Email is already taken.");//THIS EMAIL IS TAKEN!
+                txtEmail.setError("This email is already taken");//THIS EMAIL IS TAKEN!
                 txtEmail.requestFocus();
             }
         }
--- app/src/main/java/seniorproject/smartstocks/RegistrationPersonalInfoActivity.java
+++ app/src/main/java/seniorproject/smartstocks/RegistrationPersonalInfoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,57 +14,111 @@ import seniorproject.smartstocks.Classes.User;
 
 public class RegistrationPersonalInfoActivity extends AppCompatActivity {
 
+    EditText txtFistName;
+    EditText txtLastName;
+    Spinner txtAccountType;
+    EditText txtPhone;
+    EditText txtDOB;
+    Button btnRegister;
     private UserRegistrationSubmit AuthTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_personal_info);
-        final Intent previousIntent = getIntent();
 
-        EditText txtFistName = (EditText) findViewById(R.id.txtFirstName);
-        EditText txtLastName = (EditText) findViewById(R.id.txtLastName);
-        Spinner txtAccountType = (Spinner) findViewById(R.id.ddAccountType);
-        EditText txtPhone = (EditText) findViewById(R.id.txtPhone);
-        EditText txtDOB = (EditText) findViewById(R.id.txtDOB);
 
-        Button btnSubmit = (Button) findViewById(R.id.btnSubmit);
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
+        txtFistName = (EditText) findViewById(R.id.txtFirstName);
+        txtLastName = (EditText) findViewById(R.id.txtLastName);
+        txtAccountType = (Spinner) findViewById(R.id.ddAccountType);
+        txtPhone = (EditText) findViewById(R.id.txtPhone);
+        txtDOB = (EditText) findViewById(R.id.txtDOB);
+
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                User newUser =  new User();
-
-                String email = previousIntent.getStringExtra("Email");
-                String password = previousIntent.getStringExtra("Password");
-
-
-                AuthTask = new UserRegistrationSubmit(newUser);
-                AuthTask.execute((Void) null);
+                attemptUserCreation();
 
             }
         });
 
     }
 
-    private boolean attemptUserCreation(){
+    private void attemptUserCreation(){
+
+        boolean cancel = false;
+        View focusView = null;
+
+        Intent previousIntent = getIntent();
+        String email = previousIntent.getStringExtra("Email");
+        String password = previousIntent.getStringExtra("Password");
+
+        String firstname = txtFistName.getText().toString();                //Stores values at registration attempt
+        String lastname = txtLastName.getText().toString();
+       // String accountType = txtAccountType.getSelectedItem().toString();
+        //String phone = txtPhone.getText().toString();
+       // String dateOfBirth = txtDOB.getText().toString();
+
+        if (TextUtils.isEmpty(firstname)) {
+            txtFistName.setError(getString(R.string.error_field_required));
+            focusView = txtFistName;
+            cancel = true;
+        } else if (!isNameValid(firstname)) {
+            txtFistName.setError("Invalid fist name");
+            focusView = txtFistName;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(lastname)) {
+            txtLastName.setError(getString(R.string.error_field_required));
+            focusView = txtLastName;
+            cancel = true;
+        } else if (!isNameValid(lastname)) {
+            txtLastName.setError("Invalid last name");
+            focusView = txtLastName;
+            cancel = true;
+        }
 
 
-        return true;
+
+        if(cancel){
+            //will not execute registration
+        }else {
+            User user = new User();
+            AuthTask = new UserRegistrationSubmit(user);
+            AuthTask.execute((Void) null);
+        }
     }
+
+    private boolean isNameValid(String name) {
+        //TODO: Replace this with your own logic
+        return (!name.contains("\\d+") || !name.contains("\\W+"));
+    }
+
+
+
 
     public class UserRegistrationSubmit extends AsyncTask<Void, Void, Boolean>{
 
+        User newUser;
         public UserRegistrationSubmit(User user){
-
+            newUser = user;
         }
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            return null;
+            return true;
         }
 
-        protected void onPostExecute(final Boolean success) {}
+        protected void onPostExecute(final Boolean success) {
+
+            if(success){
+
+            }
+
+        }
 
         protected void onCancelled() {
             AuthTask = null;
