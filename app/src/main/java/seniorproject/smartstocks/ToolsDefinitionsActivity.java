0664f21785af3f6d690f2423347e9d33eb8bae66
ampirolli <ampirolli831@gmail.com>
2016-08-22 19:56:10 -0400

    accounts pages is making great progress 99.99% done.  Search is working. just need to reformat and launch stock info page. NEED TO START BUY AND SELL PAGE VERY SOON

--- app/src/main/java/seniorproject/smartstocks/ToolsDefinitionsActivity.java
+++ app/src/main/java/seniorproject/smartstocks/ToolsDefinitionsActivity.java
@@ -1,14 +1,72 @@
 package seniorproject.smartstocks;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import seniorproject.smartstocks.Classes.Session;
 
 public class ToolsDefinitionsActivity extends AppCompatActivity {
+
+    Session currentSession;
+    ListView lvDefinitions;
+
+    private getDefinitionsListTask AuthTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_definitions);
+        setContentView(R.layout.activity_tools_definitions_selected);
+
+        Intent previousIntent = getIntent();
+        currentSession = Session.getInstance(previousIntent.getIntExtra("Session", 0));  //loads current session into intent
+        currentSession.getUser_id();
+
+        lvDefinitions = (ListView) findViewById(R.id.lvDefinitionsList);
+    }
+
+    public class getDefinitionsListTask extends AsyncTask<Void, Void, Boolean> {
+
+        List<String> DefinitionsList= new ArrayList<String>();
+
+        public getDefinitionsListTask() {
+
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            //CONNECT TO SQL DATABASE ON SEPERATE THREAD FROM THE MAIN THREAD
+            //Load into definitions list
+
+            return true;
+        }
+        @Override
+        protected void onPostExecute(final Boolean success) {
+
+            AuthTask = null;
+
+            if(success){
+                //Load the definitions data
+
+
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            AuthTask = null;
+
+        }
+
+
     }
 
     @Override
--- app/src/main/java/seniorproject/smartstocks/ToolsDefinitionsSelectedActivity.java
+++ app/src/main/java/seniorproject/smartstocks/ToolsDefinitionsSelectedActivity.java
@@ -1,14 +1,97 @@
 package seniorproject.smartstocks;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import seniorproject.smartstocks.Classes.Account;
+import seniorproject.smartstocks.Classes.Session;
+import seniorproject.smartstocks.Classes.User;
 
 public class ToolsDefinitionsSelectedActivity extends AppCompatActivity {
+    Session currentSession;
+    ListView lvDefinitions;
+
+    private getDefinitionsTask AuthTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tools_definitions_selected);
+
+        Intent previousIntent = getIntent();
+        currentSession = Session.getInstance(previousIntent.getIntExtra("Session", 0));  //loads current session into intent
+        currentSession.getUser_id();
+
+        lvDefinitions = (ListView) findViewById(R.id.lvDefinitionsList);
+
+    }
+
+
+    public class getDefinitionsTask extends AsyncTask<Void, Void, Boolean> {
+
+        List<String> DefinitionsList= new ArrayList<String>();
+
+        public getDefinitionsTask() {
+
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            //CONNECT TO SQL DATABASE ON SEPERATE THREAD FROM THE MAIN THREAD
+            //Load into definitions list
+
+            return true;
+        }
+        @Override
+        protected void onPostExecute(final Boolean success) {
+
+            AuthTask = null;
+
+            if(success){
+                //load the spinner with a list of accounts
+
+
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(ToolsDefinitionsSelectedActivity.this, android.R.layout.simple_spinner_item, DefinitionsList);
+                arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                lvDefinitions.setAdapter(arrayAdapter);
+
+                lvDefinitions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int index, long l) {
+
+                        Intent i = new Intent(ToolsDefinitionsSelectedActivity.this, ToolsDefinitionsActivity.class); //creates intent that launches Definitions
+                        i.putExtra("Session", currentSession.getUser_id());
+                        startActivity(i);
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+                        // do nothing
+                    }
+                });
+
+
+
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            AuthTask = null;
+
+        }
+
+
     }
 
     @Override
