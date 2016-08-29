b5411868f3dac36032c40e1d107e6c9fa82694b7
ampirolli <ampirolli831@gmail.com>
2016-08-29 16:30:48 -0400

    accounts page is done (with the exception of the onActivity Result.) onto buy/sell, . we need a graph visualizer working by the end of the week LAST THING WILL BE LINKING ETRADE Definitions done

--- app/src/main/java/seniorproject/smartstocks/Classes/UserStock.java
+++ app/src/main/java/seniorproject/smartstocks/Classes/UserStock.java
@@ -2,7 +2,13 @@ package seniorproject.smartstocks.Classes;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import seniorproject.smartstocks.LICS;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
@@ -50,6 +56,48 @@ public class UserStock {
 
     public void setAccountID(Integer accountID) {
         AccountID = accountID;
+    }
+
+    public BigDecimal getPricePaid(){
+        LICS loginConnectionString = new LICS();
+        String connectionUrl = loginConnectionString.LoginConnectionString();
+
+        // Declare the JDBC objects.
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet result = null;
+
+        BigDecimal pricePaid = new BigDecimal(0);
+
+        try {
+            // Establish the connection.
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            conn = DriverManager.getConnection(connectionUrl);
+            // Create and execute an SQL statement that returns some data.
+
+            String SQL = "execute sp_get_price_paid '"+ TransactionID +"';";
+            stmt = conn.createStatement();
+            result = stmt.executeQuery(SQL);
+            int counter = 0;
+            while (result.next()) {
+
+                pricePaid = result.getBigDecimal("price_paid");
+            }
+
+
+
+        }
+
+        // Handle any errors that may have occurred.
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (result != null) try { result.close(); } catch(Exception e) {}
+            if (stmt != null) try { stmt.close(); } catch(Exception e) {}
+            if (conn != null) try { conn.close(); } catch(Exception e) {}
+            return pricePaid;
+        }
     }
 
 
